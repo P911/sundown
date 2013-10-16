13dc2a6ae603bc8d7104d7fc5dfe67d4c8fd4edd
Patrick <p@h>
2013-10-16 22:13:19 +0200

    slice-object and function, documentation

--- examples/sdoc.c
+++ examples/sdoc.c
@@ -1,12 +1,10 @@
 
-/* Sundown library, for the Markdown processing */
 #include "markdown.h"
 #include "html.h"
 /* use sundown utility for string handling */
 #include "buffer.h"
 #include "stack.h"
 
-/* usual C std library */
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,27 +18,216 @@
 #define OUTPUT_UNIT 64
 
 /**
-  Simple-Doc extension: add a (HTML) reference for
-  level 3 headers that contain a '%' or a '&'.
+### slice struct ###
 
-  **parameters**
-
-  + cb
-    * copy buffer, where the HTML-tag is added
-  + idx
-    * index of the current position in the copy buffer
+Represents a part of a buffer.
 */
-unsigned int
-add_attrib(struct buf *cb, unsigned int idx, const char *ty,
-	struct buf *in, unsigned int i_start, unsigned int i_len) {
+struct slice {
+	unsigned int pos;
+	unsigned int len;
+	struct buf *buf;
+};
 
-	memcpy(cb->data + idx, "{", 1); idx++;
-	memcpy(cb->data + idx, ty, strlen(ty)); idx += strlen(ty);
-	memcpy(cb->data + idx, in->data + i_start, i_len); idx += i_len;
-	memcpy(cb->data + idx, "}", 1); idx++;
-	return idx;
+/**
+### slice_print ###
+
+Print contents of a slice.
+
+#### parameters ####
+
++ s
+  * slice to print
+
+*/
+void slice_print(struct slice *s) {
+	fprintf(stderr, "slice:[[");
+	fwrite(s->buf->data + s->pos, 1, s->len, stderr);
+	fprintf(stderr, "]]\n");
 }
 
+/**
+### slice_set ###
+
+Initialize a slice as substring of a buffer
+
+#### parameters ####
+
++ s
+  * slice struct (typically on the stack)
++ buf
+  * buffer object
++ p
+  * start position (offset) of the slice in the buffer
++ l
+  * length of the slice string
+
+*/
+static void slice_set(struct slice *s,
+	struct buf *buf, unsigned int p, unsigned int l) {
+	s->buf = buf;
+	s->pos = p;
+	s->len = l;
+}
+
+/**
+### slice_substring ###
+
+Reset slice to be a substring of the current slice string.
+
+#### parameters ####
+
++ s
+  * slice to be modified
++ from
+  * start offset for the substring (0-based)
++ len
+  * length of the substring; if 0, use the complete rest of the string
+*/
+static void
+slice_substring(struct slice *s, unsigned int from, unsigned int len) {
+	assert(from < s->len);
+	s->len -= from;
+	s->pos += from;
+	if (len > 0) {
+		s->len = len;
+	}
+}
+
+/**
+### slice_trim ###
+
+Remove leading and trailing whitespace from a slice.
+
+#### parameters ####
+
++ s
+  * slice object
+*/
+static void slice_trim(struct slice *s) {
+	unsigned int i;
+	/* remove whitespace at the beginning of the slice */
+	for (i = s->pos; i < s->pos + s->len; ++i) {
+		if (!isspace(s->buf->data[i])) {
+			break;
+		}
+	}
+	s->len = s->len - (i - s->pos);
+	s->pos = i;
+	/* remove whitespace at the end of the slice */
+	for (i = s->pos + s->len - 1; i >= s->pos; --i) {
+		if (!isspace(s->buf->data[i])) {
+			break;
+		}
+	}
+	s->len = i - s->pos + 1;
+}
+
+/**
+### slice_find ###
+
+Find the first occurance of one of a set of characters in a slice string.
+
+#### parameters ####
+
++ s
+  * slice object
++ clist
+  * character array (NULL-terminated), representing the list of
+    characters to search for
+
+#### returns ####
+
+Position of the first occurance of one of the characters in the
+slice string.
+*/
+static int slice_find(struct slice *s, const char *clist) {
+	unsigned int i, j;
+	for (i = s->pos; i < s->pos + s->len; ++i) {
+		for (j = 0; clist[j] != 0; ++j) {
+			if (s->buf->data[i] == clist[j]) {
+				return i - s->pos;
+			}
+		}
+	}
+	return -1;
+}
+
+/**
+### slice_starts_with_id ###
+
+Determines the longest prefix of the slice representing an
+identifier. That is the longest prefix of the slice consisting
+of alphanumeric und unterscore characters.
+
+#### parameters ####
+
++ s
+  * slice object
+
+#### returns ####
+
+Number of characters of the id-prefix. (0 if there is no
+such prefix.)
+*/
+static unsigned int slice_starts_with_id(struct slice *s) {
+	unsigned int i;
+	for (i = s->pos; i < s->pos + s->len; i++) {
+		int ch = s->buf->data[i];
+		if (!isdigit(ch) && !isalpha(ch) && ch != '_') {
+			break;
+		}
+
+	}
+	return i - s->pos;
+}
+
+/**
+### add_attrib ###
+
+Simple-Doc extension: add a header attributes via
+the markdown attribut extention ("## ... {#<id>|.<class>}").
+
+####parameters####
+
++ cb
+  * copy buffer, where the attribute is added
++ ty
+  * attribut type: '.' for class or '#' for id
++ slice
+  * slice buffer for the class/id string
+
+####returns####
+
+new current position for the copy buffer
+*/
+void
+add_attrib(struct buf *cb, const char *ty, struct slice *slice) {
+
+	/* {#<str>} or {.<str>} */
+	bufputs(cb, "{");
+	bufputs(cb, ty);
+	bufput(cb, slice->buf->data + slice->pos, slice->len);
+	bufputs(cb, "}");
+}
+
+/**
+### read_files ###
+
+Read the contents of all files into the input buffer.
+
+####parameters####
+
++ ib
+  * "input buffer" (to be filled with the contents of then input file)
++ argc
+  * number of input file names
++ argv
+  * list of input file names
+
+####returns####
+
+0 on success, 1 on error
+*/
 static int read_files(struct buf *ib, int argc, char **argv) {
 	int j;
 	/* TODO: for each file (sort?) */
@@ -64,10 +251,25 @@ static int read_files(struct buf *ib, int argc, char **argv) {
 	return 0;
 }
 
-static int put_char(struct buf *cb, int idx_out, int ch) {
+/**
+### put_char ###
+
+Add a character to a buffer. If it is an ansii charactor, just
+add it. If not, guess if it is utf-8 or latin1-encoding (assuming
+the language is german). It it is not already utf-8, convert
+it to utf-8.
+
+#### parameters ####
+
++ cb
+  * output buffer to write to
++ ch
+  * the character (ansii, utf-8, or latin1 encoding)
+*/
+static void put_char(struct buf *cb, int ch) {
 	if (ch < 128) {
-		assert((unsigned int) idx_out < cb->asize);
-		cb->data[idx_out++] = ch;
+		/* ansii: just add the char */
+		bufputc(cb, ch);
 	}
 	else {
 		/* guess encodung (german) */
@@ -78,25 +280,52 @@ static int put_char(struct buf *cb, int idx_out, int ch) {
 		/*Uu*/ case 220: case 252:
 		/*sz*/ case 223:
 			/* seems to by latin-1 */
-			cb->data[idx_out++] = 0xc2 + (ch>0xbf);
-			cb->data[idx_out++] = (ch&0x3f) + 0x80;
+			bufputc(cb, 0xc2 + (ch>0xbf));
+			bufputc(cb, (ch&0x3f) + 0x80);
 			break;
 		default: /* hopefully already Utf-8 */
-			cb->data[idx_out++] = ch;
+			bufputc(cb, ch);
 			break;
 		}
 	}
-	return idx_out;
 }
 
-// API comments: starting with /** and ending with */ or **/
-// copy parts between comments
+/**
+### copy_comments ###
+
+Copy (API) comment blocks from the input buffer to the output buffer.
+
+Only comment blocks starting with '**' are copied.
+It is assued that the comment blocks are formated in the markdown
+markup language.
+
+Header-3 and Header-4 elements are treated special:
+
+* For Header-3 texts, try to find the descriped element/function
+  an use the identifier as the HTML-ID of the HTML header tag.
+* For Header-4 texts, use the text as HTML-class of the HTML header tag. 
+
+The idea is to use Header-3 with the function name and Header-4 for
+standardized sections (like "parameters", "returns", etc.).
+
+#### parameters ####
+
++ cb
+  * output buffer
++ ib
+  * input buffer
+*/
 static void copy_comments(struct buf *cb, struct buf *ib) {
-	unsigned int i, idx_out = 0;
+	unsigned int i;
 	int api_comment_block = 0, api_pound = 0, is_header = 0;
+
+	/* read input char by char */
 	for (i = 0; i < ib->size; i++) {
+
+		/* read current character */
 		int ch = ib->data[i];
-		// API comment block starts
+
+		// check if API comment block starts
 	  	if (ch == '/' && ib->size - 2 > i) {
 	    		if (ib->data[i + 1] == '*' && ib->data[i + 2] == '*') {
 	    			api_comment_block = 1;
@@ -104,81 +333,119 @@ static void copy_comments(struct buf *cb, struct buf *ib) {
 				continue;
 	    		}
 	  	}
-		// API comment block ends
+
+		// check if API comment block ends
 		if (ch == '*' && ib->size - 2 > i) {
 			if ((ib->data[i + 1] == '*' && ib->data[i + 2] == '/')
 				|| ib->data[i + 1] == '/') {
+				if (api_comment_block) {
+					// add newline after earch comment
+					// block to force new paragraph
+					// for markdown
+					put_char(cb, '\n');
+				}
 				api_comment_block = 0;
 				continue;
 			}
 		}
-		if (api_comment_block) {
-			// if this is a api comment: check if this is
-			// a level 3 heading
-			if (ch == '#') ++api_pound;
 
-			if (api_pound == 3 && ch != '#') {
-				is_header = 3;
-			}
-			if (is_header == 3 && ch == '#') {
-				// 1. remove 3 '#'
-				i += 2;
-				// 2. add {#<name>}
-				bufgrow(cb, cb->asize + 100);
-				unsigned int k, l;
-				// 2a. skip back to '&' or '%'
-				for (k = i; k >= 0; k--) {
-					if (ib->data[k] == '&') break;
-					if (ib->data[k] == '%') break;
-					if (ib->data[k] == '\n') break;
-				}
-				// 2b. find end of identifier
-				for (l = k + 1; l < i; l++) {
-					int ch = ib->data[l];
-					if (!isdigit(ch) && !isalpha(ch) && ch != '_') {
-						break;
-					}
-				}
-				// 2c. add string to out buffer
-				if (ib->data[k] == '&' || ib->data[k] == '%') {
-					idx_out = add_attrib(cb, idx_out, "#",
-						ib, k+1, l-k);
-				}
-			}
+		/* if not within (api) comment block: read next char */
+		if (!api_comment_block) continue;
 
-			if (api_pound == 4 && ch != '#') {
-				is_header = 4;
-			}
-			if (is_header == 4 && ch == '#') {
-				// 1. add {.<header>}
-				bufgrow(cb, cb->asize + 100);
-				unsigned int k;
-				// 1a. skip back to '#'
-				for (k = i - 1; k >= 0; k--) {
-					if (ib->data[k] == '#') break;
-					if (ib->data[k] == '\n') break;
-				}
-				// 1c. add string to out buffer
-				if (ib->data[k] == '#') {
-					idx_out = add_attrib(cb, idx_out, ".",
-						ib, k+1, i-k-1);
-				}
-				// 2. remove 4 '#'
-				i += 3;
-			}
+		/* if api comment:
+		   - check for (specially treater) headings
+		   - write current char to output
+		*/
 
-			if (api_pound && ch != '#') api_pound = 0;
-			if (ch == '\n') is_header = 0;
+		// if this is a api comment: check if this is
+		// a level 3 heading
+		if (ch == '#') ++api_pound;
 
-			idx_out = put_char(cb, idx_out, ch);
-
+		if (api_pound == 3 && ch != '#') {
+			is_header = 3;
+			/* state: in header line */
 		}
+		if (is_header == 3 && (ch == '#' || ch == '\n')) {
+			struct slice slice;
+			/* state: in header line and at end of header text */
+			// 1. add {#<name>}
+			unsigned int k;
+			for (k = i - 1; k >= 0; k--) {
+				if (ib->data[k] == '#') break;
+			}
+			slice_set(&slice, ib, k + 1, i - k - 1);
+
+			slice_trim(&slice);
+
+			if (slice_find(&slice, "&%") != -1) {
+				// if the string contains '&'/'%':
+				// use the identifier as ID
+				// remove the '&'/'%'
+				slice_substring(&slice,
+					slice_find(&slice, "&%") + 1, 0);
+			}
+
+			slice_substring(&slice, 0,
+				slice_starts_with_id(&slice));
+			// 1c. add string to out buffer
+			add_attrib(cb, "#", &slice);
+			// 2. remove 3 '#'
+			if (ch == '#') i += 2;
+			// 3. reset header
+			api_pound = 0;
+			is_header = 0;
+			continue;
+		}
+
+		if (api_pound == 4 && ch != '#') {
+			is_header = 4;
+		}
+		if (is_header == 4 && ch == '#') {
+			struct slice slice;
+			// 1. add {.<header>}
+			// 1a. skip back to '#'
+			unsigned int k;
+			for (k = i - 1; k >= 0; k--) {
+				if (ib->data[k] == '#') break;
+			}
+			slice_set(&slice, ib, k + 1, i - k - 1);
+			slice_trim(&slice);
+			// 1c. add string to out buffer
+			add_attrib(cb, ".", &slice);
+			// 2. remove 4 '#'
+			i += 3;
+			// 3. reset header
+			api_pound = 0;
+			is_header = 0;
+			continue;
+		}
+
+		if (api_pound && ch != '#') api_pound = 0;
+		if (ch == '\n') is_header = 0;
+
+		put_char(cb, ch);
 	}
-	cb->size = idx_out;
 }
 
+/**
+### doc_api ###
+
+Process API-comments from an source files and produce HTML
+from the Markdown in the comments.
+
+#### parameters ####
+
++ ib
+  * input buffer
++ out
+  * output file (where the HTML should be written)
+
+#### returns ####
+
+0 on success, -1 on error
+*/
 static int
-doc_api(struct buf *ib)
+doc_api(struct buf *ib, FILE *out)
 {
 	struct buf *ob;
 	struct buf *cb;
@@ -207,13 +474,13 @@ doc_api(struct buf *ib)
 	sd_markdown_free(markdown);
 
 	/* HTML-header */
-	fprintf(stdout, "<html><meta charset='utf-8'>\n");
-	fprintf(stdout, "<head><link href=\"apidoc.css\" "
+	fprintf(out, "<html><meta charset='utf-8'>\n");
+	fprintf(out, "<head><link href=\"apidoc.css\" "
 	                "rel=\"stylesheet\" type=\"text/css\">");
-	fprintf(stdout, "</head><body>");
-	fprintf(stdout, "<div id=\"Content\">");
-	ret = fwrite(ob->data, 1, ob->size, stdout);
-	fprintf(stdout, "</div>");
+	fprintf(out, "</head><body>");
+	fprintf(out, "<div id=\"Content\">");
+	ret = fwrite(ob->data, 1, ob->size, out);
+	fprintf(out, "</div>");
 
 	/* cleanup */
 	bufrelease(ob);
@@ -355,7 +622,7 @@ main(int argc, char **argv)
 	}
 
 	if (strcmp(argv[1], "-api") == 0) {
-		doc_api(ib);
+		doc_api(ib, stdout);
 	}
 	else {
 		docco(ib);
